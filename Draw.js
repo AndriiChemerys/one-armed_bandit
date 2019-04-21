8ee863305e9621bcf49e1590d2ad7a45c98b77c1
AndriiChemerys <andriichemerys@gmail.com>
2019-04-21 20:06:25 +0200

    creating Draw class

--- Draw.js
+++ Draw.js
@@ -1,6 +1,21 @@
 class Draw {
     constructor() {
         this.options=['red', 'green', 'blue'];
-        let _result=['red', 'green', 'blue']
+        let _result=['red', 'green', 'blue'];
+        this.getDrawResult = () => _result;
+
+    }
+    drawResult() {
+        let colors = [];
+        // uzupełnianie poprzez losowanie
+        for (let i=0; i< this.options.length; i++) {
+            const index = Math.floor(Math.random() * this.options.length) 
+            const color = this.options[index]
+            console.log(color);
+            colors.push(color)
+        }
+        return colors
     }
 }
+
+const draw = new Draw();
