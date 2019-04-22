ee830c9676fca8eade0fa3f03ceea4322c6d6ebc
AndriiChemerys <andriichemerys@gmail.com>
2019-04-22 19:33:57 +0200

    adding func to game

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-// link do gry: https://websamuraj.pl/examples/js/gra/ 
+class Game {
+    constructor() {
+        this.stats = new Statistics();
+        this.wallet = new Wallet(100);
+        document.getElementById('start').addEventListener('click', this.startGame);
+        this.spanWallet = document.querySelcetor('.panel span.wallet');
+        this.boards = document.querySelector('div.color');
+        this.inputBid = document.getElementById('bid');
+        this.spanResult = document.querySelector('.score span.result');
+        this.spanGames = document.querySelector('.score span.number');
+        this.spanWins = document.querySelector('.score span.win');
+        this.spanLosses = document.querySelector('.score span.loss');
+
+    }
+    render() {
+
+    }
+
+    startGame() {
+
+    }
+}
